5fc8f1236975ab3b60b3bf993c2f43021e668331
PhatLavar <phat.nguyentan188@gmail.com>
2026-05-03 14:45:20 +0700

    [FEAT] 30 Days of Pandas - Data Filtering - DONE!

---
+++ 30 Days of Pandas/Data Filtering/595_Big_Countries.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    return world.loc[
+        (world["area"] >= 3e6) | (world["population"] >= 25e6),
+        ["name", "population", "area"]
+    ]
